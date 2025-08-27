00c128d1f13c2927773c30c3dcef18ec82cda545
Varun-047 <varunmarwah.2004@gmail.com>
2025-08-27 09:34:56 +0530

    merge components

--- GitaVerse-frontend/src/App.tsx
+++ GitaVerse-frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar';
+import HeroSection from './components/HeroSection';
 function App() {
 
   return (
     <>
+    <Navbar />
+    <HeroSection />
     </>
   )
 }
