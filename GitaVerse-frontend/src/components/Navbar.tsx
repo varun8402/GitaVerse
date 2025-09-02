5954061756215d5abbf4d5643d1e3225c0db3b94
Varun-047 <varunmarwah.2004@gmail.com>
2025-09-02 22:36:46 +0530

    updated nav

--- GitaVerse-frontend/src/components/Navbar.tsx
+++ GitaVerse-frontend/src/components/Navbar.tsx
@@ -1,7 +1,7 @@
 const Navbar = ()=>{
     return (
        <>
-  <div className="flex justify-between items-center p-4 z-50 w-full absolute text-white">
+  <div className="flex justify-between items-center p-4 z-50 w-full absolute text-black ">
     {/* Left Logo */}
     <div className="flex items-center gap-2">
       <img
@@ -9,33 +9,33 @@ const Navbar = ()=>{
         className="w-12 mb-2"
         alt="GitaVerse Logo"
       />
-      <span className="font-bold text-lg">GitaVerse</span>
+      <span className="font-bold text-lg ">GitaVerse</span>
     </div>
 
     {/* Right Nav */}
     <ul className="flex gap-10 items-center p-2">
       <li>
         <a
-          href="#"
+          href="/"
           className="hover:text-gray-300 cursor-pointer"
         >
           Home
         </a>
       </li>
       <li>
-        <a href="#" className="hover:text-gray-300 cursor-pointer">Browse Gita</a>
+        <a href="/browse" className="hover:text-gray-300 cursor-pointer">Browse Gita</a>
       </li>
       <li>
-        <a href="#" className="hover:text-gray-300 cursor-pointer">AI Chatbot</a>
+        <a href="#" className="hover:text-gray-300 cursor-pointer ">AI Chatbot</a>
       </li>
       <li>
-        <a href="#" className="hover:text-gray-300 cursor-pointer">Random Shloka</a>
+        <a href="#" className="hover:text-gray-300 cursor-pointer ">Random Shloka</a>
       </li>
       <li>
-        <a href="#" className="hover:text-gray-300 cursor-pointer">Contact</a>
+        <a href="#" className="hover:text-gray-300 cursor-pointer ">Contact</a>
       </li>
       <li>
-        <a href="#" className="inline-block border rounded-[20%] px-3 py-2 hover:bg-white hover:text-black cursor-pointer duration-300">Signup</a>
+        <a href="#" className="inline-block border rounded-[20%] px-3 py-2 hover:bg-white hover:text-black cursor-pointer duration-300 ">Signup</a>
       </li>
     </ul>
   </div>
