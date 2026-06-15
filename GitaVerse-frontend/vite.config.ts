ccf28cb0119167329eb0c9052be87c85c339c4f6
varun8402 <varunmarwah.2004@gmail.com>
2026-06-15 17:04:09 +0530

    Added backend integration

--- GitaVerse-frontend/vite.config.ts
+++ GitaVerse-frontend/vite.config.ts
@@ -1,11 +1,25 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: env.BACKEND_URL || 'http://localhost:3000',
+          changeOrigin:true,
+          secure: false,
+          ws: true, 
+        },
+      },
+    },
+    plugins: [
+      react(), 
+      tailwindcss()
+    ],
+  };
 })
