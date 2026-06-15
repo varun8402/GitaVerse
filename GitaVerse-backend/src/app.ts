0f57a5d8a316c08ba271b56a258a64d2d114ae92
varun8402 <varunmarwah.2004@gmail.com>
2026-06-15 17:31:16 +0530

    removed proxy and added cors

--- GitaVerse-backend/src/app.ts
+++ GitaVerse-backend/src/app.ts
@@ -1,11 +1,23 @@
 import express, {Request, Response} from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 const routes = require("./routes/routes"); 
+const cors = require("cors");
+
 
 app.use(express.json());
 app.use("",routes);
+
+
+const corsOptions = {
+  origin: process.env.FRONTEND_URL,
+};
+
+app.use(cors(corsOptions))
 
 app.get("/", (req:Request, res:Response)=>{
     res.send("Gita Verse Backend is Running");
